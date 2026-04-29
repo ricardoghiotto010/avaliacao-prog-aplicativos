0abfac695c70d84125205e3a40683b3fb1025ad7
ricardo daniel ghiotto <ricardo.ghiotto@escola.pr.gov.br>
2026-04-29 13:30:52 -0300

    quarto commit:3 FUNÇÃO

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ def pedir_valor(frase):
 def calcular_desconto(preco, cupom):
     valor_final = preco - cupom
     return valor_final
+
+def mostrar_recibo(total):
+    print(f"O valor com desconto é: R$ {total}")
+    
